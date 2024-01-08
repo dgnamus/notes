f1f93a7e923e3236326fd34fa328b083357dd2a3
David Gnamus <david.gnamus@protonmail.com>
2024-01-08 15:39:24 +0000

    Created initials models and migrated database

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 db_host = os.environ.get('DB_HOST', default='localhost')
 db_name = os.environ.get('DB_NAME', default='notes')
@@ -9,5 +12,5 @@ db_port = os.environ.get('DB_PORT', default='5432')
 # we put the false for track modifications, because we dont want warnings about it in the output
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-SQLALCHEMY_DATABASE_URI = f"postgres://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+SQLALCHEMYDATABASEURI = f"postgres://{db_user}:{db_password}@{db_name}:{db_port}/{db_name}"
 
